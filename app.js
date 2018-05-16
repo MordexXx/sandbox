ddb89f568076747e81f45d87acc5f84c8da25e22
Petri Alajoki <mordex@gmail.com>
2018-05-16 15:57:55 +0300

    1

--- app.js
+++ app.js
@@ -19,3 +19,11 @@ app.post('/', function(req, res){
   var result = req.rawBody;
   res.send("hello there world data is " + result);
 });
+
+
+//DATABASE CONNECTION
+var pg = require('pg');
+var conString = "postgres://eostjvbcswkgkx:b508c6ca0068375b0ef8526bf19f0d233ca38343629b4d18b20af5dc0d7172c8@ec2-79-125-12-48.eu-west-1.compute.amazonaws.com:5432/dahedgbqcsnn6l";
+
+var client = new pg.Client(conString);
+client.connect();
