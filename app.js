94f2e1bde97e8944d0b41d9418f17c8c58dd199f
Petri Alajoki <mordex@gmail.com>
2018-05-16 13:13:15 +0300

    1

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', function(req, res){
-    res.send('Hello world');
+    res.render('index');
 });
 
 app.listen(3000, function(){
