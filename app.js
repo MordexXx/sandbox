da75e8a5b8eab7dea4a64af0cdd2324cf950c181
Petri Alajoki <mordex@gmail.com>
2018-05-16 21:10:47 +0300

    1

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const client = new Client({
 
 client.connect();
 
-app.post('../app.js', function(req, res){
+app.post('/api/data', function(req, res){
     
     client.query('SELECT * FROM comments;', (err, res) => {
         if (err) throw err;
