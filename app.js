185c59c1920d58c005169e860841272afe569a24
Petri Alajoki <mordex@gmail.com>
2018-05-16 14:22:52 +0300

    1

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var path = require('path');
 
 var app = express();
 
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
+
 // var logger = function(req, res, next){
 //     console.log('Logging...');
 //     next();
