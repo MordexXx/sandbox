17b29e61814b3488f6b066ffedc469f1b801bc93
Petri Alajoki <mordex@gmail.com>
2018-05-17 13:41:35 +0300

    1

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ console.log(query);
 
 var comments = "<ul style=\"list-style-type: none;\">";
 
-query = 'SELECT * FROM comments';
+query = 'SELECT date::varchar,name,comment FROM comments';
 client.query(query, (err, res) => {
     if (err) throw err;
 
