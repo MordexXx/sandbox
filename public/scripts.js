7a17f11cc51d08c2c81fbfc0d3e13be5ca63dfca
Petri Alajoki <mordex@gmail.com>
2018-05-17 17:53:30 +0300

    1

--- public/scripts.js
+++ public/scripts.js
@@ -66,7 +66,7 @@ for (var i = 0; i < btns.length; i++) {
 		}
 		else if(currentPage.includes("Vieraskirja")){
 			let currentPageContent = document.getElementById("main");
-				console.log(currentPage);
+				console.log(currentPageContent);
 				if(currentPage.includes("form")){
 
 				}
