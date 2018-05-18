06b27d8b12034535b0c024637713d50395fff225
Petri Alajoki <mordex@gmail.com>
2018-05-18 04:57:13 +0300

    1

--- app.js
+++ app.js
@@ -39,8 +39,7 @@ io.on('connection', (socket) => {
     //LOG ACTIVE CONNECTIONS
     console.log('Client connected');
     //LOG DISCONNECTS
-    socket.on('disconnect', () => console.log('Client disconnected'), client.end()
-        );
+    socket.on('disconnect', () => console.log('Client disconnected'));
 
         //CONNECT TO DATABASE
         const client = new Client({
