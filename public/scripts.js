24c10d51d9120eab860b7a222a2e5cdbe167721d
Petri Alajoki <mordex@gmail.com>
2018-05-20 22:38:30 +0300

    1

--- public/scripts.js
+++ public/scripts.js
@@ -85,6 +85,11 @@ var btns = btnContainer.getElementsByClassName('menu-item');
 if(sessionStorage.key('activePage') === null){
 	currentPageContent.innerHTML = etusivuHTML;
 	sessionStorage.setItem('activePage', 'Etusivu');
+	var current = document.getElementsByClassName('active');
+	for (var i = 0; i < btns.length; i++) {
+		console.log(current);
+	}
+	
 }
 else{
 	currentPageContent.innerHTML = etusivuHTML;
