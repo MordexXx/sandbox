e3ce500d4bb40a7edc310ba0a70ec936545fb005
Petri Alajoki <mordex@gmail.com>
2018-05-20 19:53:46 +0300

    1

--- public/scripts.js
+++ public/scripts.js
@@ -2,6 +2,92 @@
 
 var currentPage;
 var socket = io();
+//FETCH MAIN ELEMENT CONTENTS
+
+
+//ETUSIVU
+var etusivuHTML;
+var getEtusivuHTML = new XMLHttpRequest();
+
+getEtusivuHTML.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		etusivuHTML = this.responseText;
+	}
+};
+
+getEtusivuHTML.open("GET", "/contents/etusivu.html", true);
+getEtusivuHTML.send();
+
+
+//YRITYS
+var yritysHTML;
+var getyritysHTML = new XMLHttpRequest();
+
+getyritysHTML.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		yritysHTML = this.responseText;
+	}
+};
+
+getyritysHTML.open("GET", "/contents/yritys.html", true);
+getyritysHTML.send();
+
+
+//PALVELUT
+var palvelutHTML;
+var getpalvelutHTML = new XMLHttpRequest();
+
+getpalvelutHTML.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		palvelutHTML = this.responseText;
+	}
+};
+
+getpalvelutHTML.open("GET", "/contents/palvelut.html", true);
+getpalvelutHTML.send();
+
+
+//YHTEYSTIEDOT
+var yhteystiedotHTML;
+var getyhteystiedotHTML = new XMLHttpRequest();
+
+getyhteystiedotHTML.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		yhteystiedotHTML = this.responseText;
+	}
+};
+
+getyhteystiedotHTML.open("GET", "/contents/yhteystiedot.html", true);
+getyhteystiedotHTML.send();
+
+
+//VIERASKIRJA
+var vieraskirjaHTML;
+var getvieraskirjaHTML = new XMLHttpRequest();
+
+getvieraskirjaHTML.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		vieraskirjaHTML = this.responseText;
+	}
+};
+
+getvieraskirjaHTML.open("GET", "/contents/vieraskirja.html", true);
+getvieraskirjaHTML.send();
+
+
+// document.addEventListener("DOMContentLoaded", function(event) {
+// var currentPageContent = document.getElementById('main');
+// currentPageContent.innerHTML = etusivuHTML;
+// });
+
+
+
+//SET ACTIVE MENU BUTTON TO ETUSIVU ON PAGE LOAD
+function loadetusivu(){
+	var currentPageContent = document.getElementById('main');
+	currentPageContent.innerHTML = etusivuHTML;
+}
+
 
 //CHECK WHICH MENU BUTTON HAS BEEN PRESSED AND SET IT AS ACTIVE
 var btnContainer = document.getElementById('navbar');
